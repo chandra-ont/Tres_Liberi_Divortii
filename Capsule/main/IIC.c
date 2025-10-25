0675ee6300db484865d98effcd33b6c44f6c3d40
Hjalmar Ålåker <hjalmara@localhost.localdomain>
2025-10-25 19:08:31 +0200

    iic

---
+++ Capsule/main/IIC.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#define I2C_MASTER_SCL_IO           19      /*!< GPIO for I2C master clock */
+#define I2C_MASTER_SDA_IO           18      /*!< GPIO for I2C master data  */
+#define I2C_MASTER_NUM              I2C_NUM_0
+#define I2C_MASTER_FREQ_HZ          100000  /*!< 100 kHz typical bus speed */
+#define I2C_MASTER_TX_BUF_DISABLE   0
+#define I2C_MASTER_RX_BUF_DISABLE   0
+#define I2C_MASTER_TIMEOUT_MS       1000
+
+
+void app_main(void)
+{
+
+}
